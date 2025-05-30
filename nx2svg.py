39f6d2e1db90efcd1779237c858aec1f9223459b
strongwire2 <Kanghyun Lee 61117236+strongwire2@users.noreply.github.com>
2025-05-30 15:40:04 +0900

    add_dots

--- nx2svg.py
+++ nx2svg.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import drawsvg as draw
 
 
 def find_longest_path(G):
@@ -17,6 +18,57 @@ def find_longest_path(G):
                     if len(path) > len(longest_path):
                         longest_path = path
     return longest_path
+
+
+def add_dots(drawing, cx, cy, text_size, dot_count, loc):
+    """
+    비공유 전자를 그린다.
+
+    :param drawing: drawSvg 객체
+    :param cx: 텍스트 중심 좌표 x
+    :param cy: 텍스트 중심 좌표 y
+    :param text_size: 텍스트 크기
+    :param dot_count: 점 수. 1 or 2
+    :param loc: t(op), r(ight), b(ottom), l(eft)
+    :return:
+    """
+    if loc == 't':
+        # 윗 점
+        drawing.append(draw.Circle(cx - 3, cy - text_size / 2 - 4, 2))
+        if dot_count == 2:
+            drawing.append(draw.Circle(cx + 3, cy - text_size / 2 - 4, 2))
+    elif loc == 'r':
+        # 오른쪽
+        drawing.append(draw.Circle(cx + text_size / 2 + 1, cy - 2 - 3, 2))
+        if dot_count == 2:
+            drawing.append(draw.Circle(cx + text_size / 2 + 1, cy - 2 + 3, 2))
+    elif loc == 'b':
+        # 아래쪽
+        drawing.append(draw.Circle(cx - 3, cy + text_size / 2 + 1, 2))
+        if dot_count == 2:
+            drawing.append(draw.Circle(cx + 3, cy + text_size / 2 + 1, 2))
+    elif loc == 'l':
+        # 왼쪽
+        drawing.append(draw.Circle(cx - text_size / 2 - 1, cy - 2 - 3, 2))
+        if dot_count == 2:
+            drawing.append(draw.Circle(cx - text_size / 2 - 1, cy - 2 + 3, 2))
+
+
+def test_svg():
+    text_size = 24
+    node_gap = 12
+
+    d = draw.Drawing(100, 100)
+
+    cx = 20
+    cy = 30
+    d.append(draw.Text("H", text_size, cx, cy, center=True, dominant_baseline='middle'))
+    add_dots(d, cx, cy, text_size, 2, 't')
+    add_dots(d, cx, cy, text_size, 2, 'r')
+    add_dots(d, cx, cy, text_size, 2, 'b')
+    add_dots(d, cx, cy, text_size, 2, 'l')
+    d.save_svg('example.svg')
+    #d.rasterize()  # Display as PNG
 
 
 def test_find_longest_path():
@@ -39,4 +91,4 @@ def test_find_longest_path():
 
 if __name__ == '__main__':
     test_find_longest_path()
-
+    test_svg()
